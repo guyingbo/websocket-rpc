c10811c74d539cafaf0ea591e4eadcc30a6c7e07
Gu Yingbo <guyingbo@papayamobile.com>
2018-05-29 11:44:06 +0800

    make some methods private

--- wsrpc/__init__.py
+++ wsrpc/__init__.py
@@ -15,22 +15,26 @@ class RemoteCallError(Exception):
 
 
 class NotifyProxy:
+    __slots__ = ('rpc',)
+
     def __init__(self, rpc):
         self.rpc = rpc
 
     def __getattr__(self, name):
         async def func(*args):
-            await self.rpc.send_notify(name, args)
+            await self.rpc._send_notify(name, args)
         return func
 
 
 class RequestProxy:
+    __slots__ = ('rpc',)
+
     def __init__(self, rpc):
         self.rpc = rpc
 
     def __getattr__(self, name):
         async def func(*args):
-            return await self.rpc.send_request(name, args)
+            return await self.rpc._send_request(name, args)
         return func
 
 
@@ -43,45 +47,45 @@ class WebsocketRPC:
                  http_request=None):
         self.ws = ws
         self.timeout = timeout
-        self.packer = msgpack.Packer(use_bin_type=1)
+        self._packer = msgpack.Packer(use_bin_type=1)
         self._request_table = {}
-        self.tasks = set()
+        self._tasks = set()
         self.notify = NotifyProxy(self)
         self.request = RequestProxy(self)
-        self.iter = itertools.count()
+        self._iter = itertools.count()
         self.client_mode = client_mode
         self.max_id = 2 ** 32
         self.http_request = http_request
         self.handler = handler_cls(self) if handler_cls else None
-        self.exc_handlers = []
+        self._exc_handlers = []
         if self.client_mode:
             asyncio.ensure_future(self.run())
 
-    def next_msgid(self):
-        i = next(self.iter)
+    def _next_msgid(self):
+        i = next(self._iter)
         if i < self.max_id:
             return i
-        self.iter = itertools.count()
-        return self.next_msgid()
+        self._iter = itertools.count()
+        return self._next_msgid()
 
     async def run(self):
         async for data in self.ws:
             try:
-                await self.on_data(data)
+                await self._on_data(data)
             except Exception as e:
                 logger.exception(e)
-                for handler in self.exc_handlers:
-                    if asyncio.iscoroutinefunction(handler):
-                        await handler(e)
+                for exc_handler in self._exc_handlers:
+                    if asyncio.iscoroutinefunction(exc_handler):
+                        await exc_handler(e)
                     else:
-                        handler(e)
-        if self.tasks:
-            await asyncio.wait(self.tasks, timeout=self.timeout)
+                        exc_handler(e)
+        if self._tasks:
+            await asyncio.wait(self._tasks, timeout=self.timeout)
 
     def exception(self, func):
-        self.exc_handlers.append(func)
+        self._exc_handlers.append(func)
 
-    async def on_data(self, data):
+    async def _on_data(self, data):
         msg = msgpack.unpackb(data, encoding='utf-8')
         assert type(msg) == list, 'unknown message format'
         assert len(msg) > 0, 'error message length'
@@ -90,60 +94,60 @@ class WebsocketRPC:
             msgid, method_name, params = msg[1:]
             method_name = method_name
             task = asyncio.ensure_future(
-                    self.on_request(msgid, method_name, params))
+                    self._on_request(msgid, method_name, params))
         elif msgtype == self.RESPONSE:
             msgid, error, result = msg[1:]
-            self.on_response(msgid, error, result)
+            self._on_response(msgid, error, result)
             task = None
         elif msgtype == self.NOTIFY:
             method_name, params = msg[1:]
             method_name = method_name
-            task = asyncio.ensure_future(self.on_notify(method_name, params))
+            task = asyncio.ensure_future(self._on_notify(method_name, params))
         else:
             raise RPCError('unknown msgtype')
         if task:
-            self.tasks.add(task)
-            task.add_done_callback(self.tasks.remove)
+            self._tasks.add(task)
+            task.add_done_callback(self._tasks.remove)
 
-    async def on_request(self, msgid, method_name, params):
+    async def _on_request(self, msgid, method_name, params):
         try:
             method = getattr(self.handler, method_name)
             result = method(*params)
             if asyncio.iscoroutine(result):
                 result = await result
         except Exception as e:
-            await self.send_response(msgid, 1, str(e))
+            await self._send_response(msgid, 1, str(e))
         else:
-            await self.send_response(msgid, 0, result)
+            await self._send_response(msgid, 0, result)
 
-    def on_response(self, msgid, error, result):
+    def _on_response(self, msgid, error, result):
         fut = self._request_table.pop(msgid)
         if error == 0:
             fut.set_result(result)
         else:
             fut.set_exception(RemoteCallError(error, result))
 
-    async def on_notify(self, method_name, params):
+    async def _on_notify(self, method_name, params):
         method = getattr(self.handler, method_name)
         result = method(*params)
         if asyncio.iscoroutine(result):
             result = await result
 
-    async def send_response(self, msgid, error, result):
+    async def _send_response(self, msgid, error, result):
         message = [self.RESPONSE, msgid, error, result]
-        data = self.packer.pack(message)
+        data = self._packer.pack(message)
         await self.ws.send(data)
 
-    async def send_request(self, method, params):
-        msgid = self.next_msgid()
+    async def _send_request(self, method, params):
+        msgid = self._next_msgid()
         message = [self.REQUEST, msgid, method, params]
-        data = self.packer.pack(message)
+        data = self._packer.pack(message)
         fut = asyncio.Future()
         self._request_table[msgid] = fut
         await self.ws.send(data)
         return await fut
 
-    async def send_notify(self, method, params):
+    async def _send_notify(self, method, params):
         message = [self.NOTIFY, method, params]
-        data = self.packer.pack(message)
+        data = self._packer.pack(message)
         await self.ws.send(data)
