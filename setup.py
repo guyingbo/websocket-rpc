1e56d351063aa1f3bb89658f4afb2210b2a28cb1
Gu Yingbo <guyingbo@papayamobile.com>
2018-06-15 16:01:08 +0800

    readme

--- setup.py
+++ setup.py
@@ -15,9 +15,15 @@ with open(os.path.join(BASE_PATH, 'wsrpc', '__init__.py')) as f:
         raise RuntimeError('Unable to determine version.')
 
 
+with open(os.path.join(BASE_PATH, 'README.md')) as readme:
+    long_description = readme.read()
+
+
 setup(
     name='websocket-rpc',
     description='msgpack rpc over websocket',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     version=version,
     author='Yingbo Gu',
